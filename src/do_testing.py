291ae8b20f2b2cb6f08545fc3fd737745ade267a
Gregory Izatt <gregory.izatt@gmail.com>
2018-10-15 20:08:12 -0400

    Saving lots of results files

--- src/do_testing.py
+++ src/do_testing.py
@@ -69,73 +69,112 @@ if __name__ == "__main__":
     instance_matcher = re.compile("[0-9][0-9][0-9].pc")
 
     # Go through all scenes...
-    for scene_foldername in os.listdir("data"):
-        if scene_name_matcher.match(scene_foldername):
-            scene_dir = os.path.join("data", scene_foldername)
-            # Run on this scene! Load in the configuration YAML...
-            config = yaml.load(open(os.path.join(
-                scene_dir, "scene_description.yaml")))
+    try:
+        for scene_foldername in os.listdir("data"):
+            if scene_name_matcher.match(scene_foldername):
+                scene_dir = os.path.join("data", scene_foldername)
+                # Run on this scene! Load in the configuration YAML...
+                config = yaml.load(open(os.path.join(
+                    scene_dir, "scene_description.yaml")))
 
-            # And load all model clouds
-            model_clouds_by_classname = {}
+                # And load all model clouds
+                model_clouds_by_classname = {}
 
-            for class_i, class_name in enumerate(config["objects"].keys()):
-                model_clouds_by_classname[class_name] = load_pointcloud(
-                    os.path.join(scene_dir, class_name + ".pc"))
-                print "Loaded points for model class %s." % class_name
-            for example_foldername in os.listdir(scene_dir):
-                parsed = parse.parse(example_format, example_foldername)
-                if parsed is not None:
-                    segdist, views_string = parsed
-                    views = [int(x) for x in views_string]
-                    print "Segdist ", segdist, " and views...", views
+                for class_i, class_name in enumerate(config["objects"].keys()):
+                    model_clouds_by_classname[class_name] = load_pointcloud(
+                        os.path.join(scene_dir, class_name + ".pc"))
+                    print "Loaded points for model class %s." % class_name
+                for example_foldername in os.listdir(scene_dir):
+                    parsed = parse.parse(example_format, example_foldername)
+                    if parsed is not None:
+                        segdist, views_string = parsed
+                        views = [int(x) for x in views_string]
+                        print "Segdist ", segdist, " and views...", views
 
-                    for instance_cloud_filename in os.listdir(
-                            os.path.join(scene_dir, example_foldername)):
-                        if not instance_matcher.match(instance_cloud_filename):
-                            continue
-                        instance_j = int(instance_cloud_filename.split(".")[0])
-                        class_name = config["instances"][instance_j]["class"]
-                        gt_pose = config["instances"][instance_j]["pose"]
-                        gt_tf = transformations.euler_matrix(gt_pose[3],
-                                                             gt_pose[4],
-                                                             gt_pose[5])
-                        gt_tf[:3, 3] = gt_pose[:3]
-                        scene_points, scene_points_normals = load_pointcloud(
-                            os.path.join(scene_dir, example_foldername,
-                                         instance_cloud_filename))
+                        # Open the results yaml
+                        results_yaml_pathname = os.path.join(
+                            "results", args.method_name, scene_dir,
+                            example_foldername)
+                        os.system("mkdir -p %s" % results_yaml_pathname)
+                        results_yaml_filename = os.path.join(
+                            results_yaml_pathname, "results.yaml")
+                        if os.path.isfile(results_yaml_filename):
+                            with open(results_yaml_filename, 'r') as f:
+                                results_config = yaml.load(f)
+                        else:
+                            results_config = []
 
-                        print "\t... loaded %d points for instance %d" % (
-                            scene_points.shape[1], instance_j)
+                        for instance_cloud_filename in os.listdir(
+                                os.path.join(scene_dir, example_foldername)):
+                            if not instance_matcher.match(
+                                    instance_cloud_filename):
+                                continue
+                            instance_j = int(
+                                instance_cloud_filename.split(".")[0])
+                            class_name = config["instances"][instance_j][
+                                "class"]
+                            gt_pose = config["instances"][instance_j]["pose"]
+                            gt_tf = transformations.euler_matrix(gt_pose[3],
+                                                                 gt_pose[4],
+                                                                 gt_pose[5])
+                            gt_tf[:3, 3] = gt_pose[:3]
+                            scene_points, scene_points_normals = \
+                                load_pointcloud(os.path.join(
+                                    scene_dir, example_foldername,
+                                    instance_cloud_filename))
 
-                        if vis:
-                            draw_points(
-                                vis, "", "model",
-                                scene_points,
-                                )
+                            print "\t... loaded %d points for instance %d" % (
+                                scene_points.shape[1], instance_j)
 
-                        # Finally, run the fitter!
-                        est_tf = method_fitter_handle(
-                            scene_points, scene_points_normals,
-                            model_clouds_by_classname[class_name][0],
-                            model_clouds_by_classname[class_name][1],
-                            method_params)
+                            if vis:
+                                draw_points(
+                                    vis, "", "model",
+                                    scene_points,
+                                    )
 
-                        # Compute pose TF and geodesic rotation error:
-                        euclid_dist, angle_dist = get_pose_error(est_tf, gt_tf)
-                        # print "\t.... Euclid dist: %f" % (euclid_dist)
-                        # print "\t.... Angle dist: %f" % (angle_dist)
+                            # Finally, run the fitter!
+                            est_tf = method_fitter_handle(
+                                scene_points, scene_points_normals,
+                                model_clouds_by_classname[class_name][0],
+                                model_clouds_by_classname[class_name][1],
+                                method_params)
 
-                        # Compute an earth-movers-distance-like error term --
-                        # in this case, the percent of model points that are
-                        # more than `misalignment_tol` away from the model in
-                        # its ground truth posture (using a nearest-neighbor
-                        # check).
-                        # Anythin over 10%>, when using misalignment_tol=1cm,
-                        # indicates a bad fit.
-                        earth_movers_error = get_earth_movers_error(
-                            est_tf, gt_tf,
-                            model_clouds_by_classname[class_name][0],
-                            misalignment_tol=0.01)
-                        print "\t.... Avg Earth Movers dist: %f" % (
-                            earth_movers_error)
+                            # Compute pose TF and geodesic rotation error:
+                            euclid_dist, angle_dist = get_pose_error(
+                                est_tf, gt_tf)
+                            # print "\t.... Euclid dist: %f" % (euclid_dist)
+                            # print "\t.... Angle dist: %f" % (angle_dist)
+
+                            # Compute an earth-movers-distance-like error term
+                            # -- in this case, the percent of model points
+                            # that are more than `misalignment_tol` away from
+                            # the model in its ground truth posture (using
+                            # a nearest-neighbor check).
+                            # Anythin over 10%>, when using
+                            # misalignment_tol=1cm, indicates a bad fit.
+                            earth_movers_error = get_earth_movers_error(
+                                est_tf, gt_tf,
+                                model_clouds_by_classname[class_name][0],
+                                misalignment_tol=0.01)
+                            print "\t.... Avg Earth Movers dist: %f" % (
+                                earth_movers_error)
+
+                            results_config.append({
+                                "timestamp": time.time(),
+                                "instance": int(instance_j),
+                                "model_name": str(class_name),
+                                "views": str(views),
+                                "segdist": float(segdist),
+                                "gt_tf": gt_tf.tolist(),
+                                "est_tf": est_tf.tolist(),
+                                "euclid_dist": float(euclid_dist),
+                                "angle_dist": float(angle_dist),
+                                "earth_movers_error": float(earth_movers_error)
+                                })
+
+                        with open(results_yaml_filename, 'w') as f:
+                            yaml.dump(results_config, f,
+                                      default_flow_style=False)
+    except KeyboardInterrupt:
+        print "Interrupted"
+    print "Shutting down..."
